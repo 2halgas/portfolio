7dfc6e4687e3d72c52b3a30234038842f6b8702a
2halgas <khamidulla.2halgas@gmail.com>
2022-03-31 16:42:52 +0600

    fix: nodemailer

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -27,6 +27,19 @@ async function SendMail(
     },
     secure: true,
   });
+
+  await new Promise((resolve, reject) => {
+    transporter.verify(function (error, success) {
+        if (error) {
+            console.log(error);
+            reject(error);
+        } else {
+            console.log("Server is ready to take our messages");
+            resolve(success);
+        }
+    });
+});
+
   const body = req.body;
 
   if (!body.name || !body.email || !body.message) {
@@ -42,12 +55,18 @@ async function SendMail(
     ${body.email}</p>`
   };
 
-  transporter.sendMail(mailData, function (err, info) {
-    if(err)
-      console.log(err)
-    else
-      console.log(info)
+  await new Promise((resolve, reject) => {
+    transporter.sendMail(mailData, (err, info) => {
+        if (err) {
+            console.error(err);
+            reject(err);
+        } else {
+            console.log(info);
+            resolve(info);
+        }
+    });
   });
+
   res.status(200).json(body)
 }
 
