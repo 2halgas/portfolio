0ccfd34b2f5283acd27659fe3d2983b40d87195c
2halgas <khamidulla.2halgas@gmail.com>
2022-04-01 15:34:01 +0600

    feat: change meta tags

--- src/components/layouts/meta/head/component.tsx
+++ src/components/layouts/meta/head/component.tsx
@@ -3,14 +3,14 @@ import Head from 'next/head';
 
 export const AppHead: FC = () => (
     <Head>
-        <title>Zhalgas Khamidulla</title>
+        <title>Zhalgas`s Portfolio</title>
         <meta name="description" content="My Portfolio" />
         <link rel="icon" href="/images/logo.png" />
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
         <meta name="keywords" content="" />
-        <meta property="og:title" content="Zhalgas Khamidulla" />
+        <meta property="og:title" content="Zhalgas`s Portfolio" />
         <meta property="og:description" content="Ready for work" />
-        <meta property="og:image" content="/images/logo.png" />
+        <meta property="og:image" content="https://2halgas.vercel.app/images/logo.png" />
         <meta property="og:type" content="website" />
         <meta property="og:locale" content="en_US" />
     </Head>
