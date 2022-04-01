6a30cf931589af6b3fcfcdc522c5aefc8a836432
2halgas <khamidulla.2halgas@gmail.com>
2022-04-01 17:03:31 +0600

    fix: change meta description

--- src/components/layouts/meta/head/component.tsx
+++ src/components/layouts/meta/head/component.tsx
@@ -9,7 +9,7 @@ export const AppHead: FC = () => (
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
         <meta name="keywords" content="" />
         <meta property="og:title" content="Zhalgas`s CV" />
-        <meta property="og:description" content="Ready for work" />
+        <meta property="og:description" content="Ready for new experience" />
         <meta property="og:image" content="https://2halgas.vercel.app/images/logo.png" />
         <meta property="og:type" content="website" />
         <meta property="og:locale" content="en_US" />
