1afda6a47a6832b6028b30f6e96cd4ab98e0b4bb
2halgas <khamidulla.2halgas@gmail.com>
2022-04-01 17:47:23 +0600

    fix: theme provider

--- src/components/pages/home/component.tsx
+++ src/components/pages/home/component.tsx
@@ -1,11 +1,19 @@
 import React, { FC } from 'react';
 import { AboutSection, SkillsSection, PortfolioSection, ContactSection } from 'src/components/organisms'
+import { useEffect, useState } from 'react';
 
-export const Home: FC = () => (
+export const Home: FC = () => {
+const [mounted, setMounted] = useState(false);
+
+    useEffect(() => {
+        setMounted(true)
+    }, [])
+    if (!mounted) return null;
+    return (
     <div className='container'>
         <AboutSection />
         <SkillsSection />
         <PortfolioSection />
         <ContactSection />
     </div>
-);
+)};
