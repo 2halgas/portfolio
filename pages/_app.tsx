b5c17e8c0a33939b85d59a0bde64837f4afdb462
2halgas <khamidulla.2halgas@gmail.com>
2022-04-05 00:01:14 +0600

    feat: add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,11 +7,23 @@ import store from 'src/common/store/store';
 import { AppContainer, AppHead } from 'src/components/layouts';
 import { ThemeProvider } from "next-themes";
 import {  ToastContainer, Zoom } from 'react-toastify';
+import Script from 'next/script';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
   <Provider store={store}>
       <ThemeProvider>
+        <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
+        <Script strategy="lazyOnload">
+            {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                page_path: window.location.pathname,
+                });
+            `}
+        </Script>
         <AppHead />
         <AppContainer>
       <ToastContainer transition={Zoom} />
